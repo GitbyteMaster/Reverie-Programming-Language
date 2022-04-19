df97e23a5cac673ed12d1bb55a2d05e6f708003d
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-04-19 19:58:59 -0400

    Patched Present Bugs

--- ReverieWinSetup.py
+++ ReverieWinSetup.py
@@ -2,7 +2,7 @@ import getpass
 import os
 import platform
 import time
-	
+
 path = f"/Users/{getpass.getuser()}/Desktop/"
 if platform.system() == "darwin":
     print("Reverie is now ready! Thank you for downloading. More can be found here: https://github.com/GitbyteMaster/Reverie-Programming-Language/")
@@ -13,13 +13,15 @@ else:
             print("Creating directories..")
             os.rename(f"{path}Reverie-Lang",f"{path}revfixed")
             os.mkdir(f"{path}Reverie-Lang")
-	    os.mkdir(f"{path}Reverie-Lang/struct")
+            os.mkdir(f"{path}Reverie-Lang/struct")
             print("Creating files..")
             mfile = open(f"{path}Reverie-Lang/Reverie 0.1.3.3.py","w+")
             sfile = open(f"{path}/revfixed/Reverie 0.1.3.3.py","r")
             mfile.write(sfile.read())
+            sfile.close()
+            mfile.close()
             os.mkdir(f"{path}Reverie-Lang/Assetlist")
-            sfile = open(f"{path}/revfixed/Assetlist/error.txt","r")
+            sfile = open(f"{path}/revfixed/Reverie/Assetlist/error.txt","r")
             mfile = open(f"{path}/Reverie-Lang/Assetlist/error.txt","w+")
             mfile.write(sfile.read())
             print("Reverie is now ready! Thank you for downloading. More can be found here: https://github.com/GitbyteMaster/Reverie-Programming-Language/")
