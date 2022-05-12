08dde0067a99b39000f2b25dc15e20df8089ca56
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-05-12 19:31:34 -0400

    Update ReverieInstaller.bat

--- ReverieInstaller.bat
+++ ReverieInstaller.bat
@@ -1,6 +1,4 @@
 echo Installing Reverie..
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/
-set ver=""
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/ver >> $ver
 curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.1.8.py >> /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
