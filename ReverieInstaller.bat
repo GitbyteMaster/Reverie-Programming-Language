8c292a9ba9ba7b7b5ed449d9c16b59b968631d2e
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-05-13 16:24:16 -0400

    Added cmdobj.txt

--- ReverieInstaller.bat
+++ ReverieInstaller.bat
@@ -4,5 +4,6 @@ mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/
 curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.1.8.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/
 curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/error.txt > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/error.txt
+echo empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/cmdobj.txt
 clear
 echo Installation Finished! Thank You for Downloading the Reverie Programming Language!
