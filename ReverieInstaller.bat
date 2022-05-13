4e23b02441faa32fcd023949ac662964cdb0159a
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-05-13 15:16:18 -0400

    Added Error List

--- ReverieInstaller.bat
+++ ReverieInstaller.bat
@@ -1,4 +1,8 @@
 echo Installing Reverie..
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.1.8.py >> /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
+curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.1.8.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
+mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/
+curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/error.txt > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/error.txt
+clear
+echo Installation Finished! Thank You for Downloading the Reverie Programming Language!
