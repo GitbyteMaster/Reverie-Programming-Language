ec6492a51527eb6658e44633074400e3eedaaf4c
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-06-22 21:01:47 -0400

    Ready for v0.2.1 and got correct filetype.

--- ReverieInstaller.sh
+++ ReverieInstaller.sh
@@ -1,9 +1,13 @@
 echo Installing Reverie..
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.2.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
+curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.2.1.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
 mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/
+
 curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/error.txt > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/error.txt
 echo empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/cmdobj.txt
 curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/context.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/context.py
+echo empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
+curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/revexec.sh > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/revexec.sh
+
 echo Installation Finished! Thank You for Downloading the Reverie Programming Language!
