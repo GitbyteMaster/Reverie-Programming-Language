3d8d8874fa7f027c63dae36979a83d8ddf0956a4
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-06-22 20:59:53 -0400

    Add files via upload

---
+++ main/Assetlist/revexec.sh
@@ -0,0 +1,4 @@
+function revexec {
+echo $1 > /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
+open /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Reverie\ 0.2.1.py
+}
