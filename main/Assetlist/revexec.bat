ab87c58ba38e99a5341ebb4d20284100020218a4
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-06-25 23:05:11 -0400

    Update revexec.bat

--- main/Assetlist/revexec.bat
+++ main/Assetlist/revexec.bat
@@ -1,6 +1,7 @@
-IF EXIST %1% (
 @ECHO off
+IF EXIST %1% (
 ECHO %1% > /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
+START %1%
 ) ELSE (
 ECHO '$1' doesn't exist! Are you sure you typed the right path?
 )
