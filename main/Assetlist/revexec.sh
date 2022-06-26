dac6efc7d8bbca07386f37968bca60e127a13ef8
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-06-25 22:45:40 -0400

    Update revexec.sh

--- main/Assetlist/revexec.sh
+++ main/Assetlist/revexec.sh
@@ -1,4 +1,7 @@
 function revexec {
+if [[ -f $1 ]];then
 echo $1 > /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
 open /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Reverie\ 0.2.1.py
+else
+echo "\'$1\' doesn't exist! Are you sure you typed the right path?"
 }
