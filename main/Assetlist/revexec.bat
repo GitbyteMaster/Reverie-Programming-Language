0d0e92d5e3323da053f8a28b4f5ab0e75e505703
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-07-22 09:42:36 -0400

    Update revexec.bat

--- main/Assetlist/revexec.bat
+++ main/Assetlist/revexec.bat
@@ -1,7 +1,22 @@
 @ECHO off
-IF EXIST %1% (
+IF "%1%"=="execute" do (
+IF EXIST %2% (
 ECHO %1% > /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
-START %1%
+START /Users/DaBros/Desktop/Reverie-Programming-Language-main/main/Reverie.py
 ) ELSE (
 ECHO '$1' doesn't exist! Are you sure you typed the right path?
+)
+)
+IF "%1%"=="install" (
+if NOT EXIST /Users/$USER/Desktop/Reverie-Programming-Language-main/main/struct (
+MKDIR /Users/$USER/Desktop/Reverie-Programming-Language-main/main/struct
+)
+IF NOT EXIST C:\Users\%USERNAME%\Desktop\Reverie-Programming-Language-main\main\struct\%1% (
+MKDIR C:\Users\%USERNAME%\Desktop\Reverie-Programming-Language-main\main\struct\%1%
+)
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/struct/%1%/%2% > C:\Users\%USERNAME%\Desktop\Reverie-Programming-Language-main\main\struct%1%\%2%
+if %ERRORLEVEL%==6 (
+cls
+ECHO The function or structure you tried to install doesn't exist! Check your spelling.
+)
 )
