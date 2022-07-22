17c0f2c3330f21d25a2b3be23ac44f75203af1c8
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-07-21 20:43:42 -0400

    Create Install.bat

---
+++ Install.bat
@@ -0,0 +1,13 @@
+ECHO Installing Reverie..
+MKDIR /Users/$USER/Desktop/Reverie-Programming-Language-main/
+MKDIR /Users/$USER/Desktop/Reverie-Programming-Language-main/main/
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie%200.2.1.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie\ 0.2.1.py
+MKDIR /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/
+
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/error.txt > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/error.txt
+ECHO empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/cmdobj.txt
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/context.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/context.py
+ECHO empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/revexec.sh > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/revexec.sh
+
+ECHO Installation Finished! Thank You for Downloading the Reverie Programming Language!
