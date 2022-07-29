2a4e472c6f6a6a5ecdbe436e0857f249164a16af
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-07-28 21:54:02 -0400

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,5 +1,6 @@
 ECHO Creating Directories..
 MKDIR C:\Users\%USERNAME%\Reverie
 MKDIR C:\Users\%USERNAME%\Reverie\src
+CURL 
 
 ECHO Installation Finished! Thank You for Downloading the Reverie Programming Language!
