f243726d021ab6a12c238e7b2bffbe8d30865ae2
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-07-29 13:48:09 -0400

    Add files via upload

---
+++ build/src/error.sh
@@ -0,0 +1,16 @@
+clear
+if [ $1 -eq 1 ];then
+	echo [!] Line $2 - Insufficient amount of parameters
+fi
+if [ $1 -eq 2 ];then
+	echo [!] Line $2 - Expected integer
+fi
+if [ $1 -eq 3 ];then
+	echo [!] Line $2 - No type assigned
+fi
+if [ $1 -eq 4 ];then
+	echo [!] Line $2 - File/Directory does not exist
+fi
+if [ $1 -eq 5 ];then
+	echo [!] Line $2 - Invalid syntax: Please close parenthesis
+fi
---
+++ build/src/func.sh
@@ -0,0 +1,43 @@
+if [ $1 -eq 1 ];then
+	if [[ "$3" == "" ]];then
+		sh /Users/$USER/Reverie/src/error.sh 3 $2
+	else
+		if [[ "$3" == "-d" ]];then
+			if [ $# -eq 4 ];then
+				echo $4
+			else
+				sh /Users/$USER/Reverie/src/error.sh 1 $2
+			fi
+		fi
+		if [[ "$3" == "-f" ]];then
+			if [ $# -eq 5 ];then
+				if [[ -f $5 ]];then
+					echo $4 > $5
+				else
+					sh /Users/$USER/Reverie/src/error.sh 4 $2
+				fi
+			else
+				sh /Users/$USER/Reverie/src/error.sh 1 $2
+			fi
+		fi
+	fi
+fi
+if [ $1 -eq 2 ];then
+	if [ $# -eq 3 ];then
+		sleep $3
+		if ! [ $? -eq 0 ];then
+			sh /Users/$USER/Reverie/src/error.sh 2 $2
+		fi
+	else
+		sh /Users/$USER/Reverie/src/error.sh 1 $2
+	fi
+fi
+if [ $1 -eq 3 ];then
+	if [ $# -eq 3 ];then
+		if ! [[ $2 =~ $int ]];then
+			open $2
+		fi
+	else
+		sh /Users/$USER/Reverie/src/error.sh 1 $2
+	fi
+fi
