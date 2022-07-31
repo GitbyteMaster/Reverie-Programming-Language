514842c929acf8a0ea6c93b221982b4d3e913688
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-07-31 12:38:55 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,8 +6,9 @@ echo "Creating Files.."
 echo > "/Users/$USER/Reverie/src/run"
 
 echo "Downloading Files.."
-curl "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3\%2B/build/main.py" > "/Users/$USER/Reverie/main.py"
-curl "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3\%2B/build/src/func.sh" > "/Users/$USER/Reverie/src/func.sh"
-curl "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3\%2B/build/src/error.sh" > "/Users/$USER/Reverie/src/error.sh"
+curl "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/main.py" > "/Users/$USER/Reverie/main.py"
+curl "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/func.sh" > "/Users/$USER/Reverie/src/func.sh"
+curl "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/error.sh" > "/Users/$USER/Reverie/src/error.sh"
 
+clear
 echo "Installation Finished! Thank You for Downloading the Reverie Programming Language!"
