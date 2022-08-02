28696a5d44002081521ce6bb0f11ffc91f4cfa92
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-08-02 13:18:55 -0700

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -7,8 +7,8 @@ ECHO > C:\Users\%USERNAME%\Reverie\main.py\run
 
 ECHO Downloading Files..
 CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/main.py > C:\Users\%USERNAME%\Reverie\main.py
-CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/func.sh > C:\Users\%USERNAME%\Reverie\src\func.sh
-CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/error.sh > C:\Users\%USERNAME%\Reverie\src\error.sh
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/func.bat > C:\Users\%USERNAME%\Reverie\src\func.bat
+CURL https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/error.bat > C:\Users\%USERNAME%\Reverie\src\error.bat
 
 CLS
 ECHO Installation Finished! Thank You for Downloading the Reverie Programming Language!
