7284f8b9e749c3018892e2e83bacfa0b0ef7ac63
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-08-12 10:42:38 -0400

    Add files via upload

--- build/src/func.bat
+++ build/src/func.bat
@@ -1,44 +1,47 @@
+@ECHO off
+
 REM put([type], [str/int/var])
 IF "%1"=="1" (
-  IF "%3" == "" (
+  IF "%~3"=="" (
     CALL C:\Users\%USERNAME%\Reverie\src\error.bat 3 %2
   ) ELSE (
     IF "%3"=="-d" (
-      IF "%5"!="" (
+      IF "%~5"=="" (
         ECHO %4
       ) ELSE (
-        CALL C:\User\%USERNAME%\Reverie\src\error.bat 1 %2
+        CALL C:\Users\%USERNAME%\Reverie\src\error.bat 1 %2
       )
     )
     IF "%3"=="-f" (
-      IF "%6"=="" (
-        IF EXIST %5 (
-          ECHO %4 > %5
+      IF "%~6"=="" (
+        IF EXIST "%5" (
+          ECHO "%4" > "%5"
         ) ELSE (
           CALL C:\Users\%USERNAME%\Reverie\src\error.bat 4 %2
         )
       ) ELSE (
-        CALL C:\Users\%USERNAME%\Reverie\src\error.bat 1 $2
+        CALL C:\Users\%USERNAME%\Reverie\src\error.bat 1 %2
       )
     )
   )
 )
 REM pause([int])
 if "%1"=="2" (
-  IF "%4"!="" (
+  IF "%~4"=="" (
     TIMEOUT %3
-    IF "%ERRORLEVEL%"!="0" (
+    IF NOT ERRORLEVEL 0 (
       CALL C:\Users\%USERNAME%\Reverie\src\error.bat 2 %2
     )
   ) ELSE (
-    CALL C:\Users\%USERNAME%\Reverie\src\error.bat 1 $2
+    ECHO %*
+    REM CALL C:\Users\%USERNAME%\Reverie\src\error.bat 1 %2
   )
 )
 REM open(str)
 IF "%1"=="3" (
-  IF "%4"!="" (
+  IF "%~4"=="" (
     START %3
-    IF "%ERRORLEVEL%"!="0" (
+    IF NOT ERRORLEVEL 0 (
       CALL C:\Users\%USERNAME%\Reverie\src\error.bat 6 %2
     )
   ) ELSE (
