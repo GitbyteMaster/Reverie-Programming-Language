84ad8c4b9e576c8cb23d0c30c6c9fd56583f8fa0
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-08-17 15:45:50 -0400

    Update main.py

--- build/main.py
+++ build/main.py
@@ -32,18 +32,23 @@ def paramcollect():
             os.system("CALL C:\Users\{getpass.getuser()}\Reverie\src\error.sh 5 {mainum}")
 mainum = 0
 literal = False
-if platform.system() == "Darwin":
+if platform.system() != "":
     os.system("clear")
 else:
     os.system("cls")
 if platform.system() != "Darwin":
     os.system(f"title {sdir[filen-1]}")
 note = False
+var = []
+data = []
 while not mainum == len(script):
     literal = False
     mainum += 1
     line = script[mainum-1]
     if not "//" in line or note:
+        callnum = 0
+        
+        # Functions in engine
         if "put(" in line:
             callnum = 1
         if "pause(" in line:
@@ -52,12 +57,23 @@ while not mainum == len(script):
             callnum = 3
         if "move(" in line:
             callnum = 4
-        
-        # Call function through engine (func)
-        paramcollect()
-        if platform.system() != "Windows":
-            os.system(f"sh /Users/$USER/Reverie/src/func.sh {callnum} {mainum} {vals}")
-        else:
-            os.system(f"CALL C:\Users\%USERNAME\Reverie\func.bat {callnum} {mainum} {vals}")
+            
+        # Call function through engine (func.bat/sh)
+        if callnum != 0:
+            paramcollect()
+            if platform.system() != "Windows":
+                os.system(f"sh /Users/$USER/Reverie/src/func.sh {callnum} {mainum} {vals}")
+            else:
+                os.system(f"CALL C:\Users\%USERNAME\Reverie\func.bat {callnum} {mainum} {vals}")
+            
+        # Functions in Python
+        if "var " in line and "=" in line:
+            paramcollect()
+            numone = line.index("var")+5
+            data = [""]
+            while not line[numone] == " ":
+                data[len(data)-1] = f"{data[len(data)-1]}{line[numone]}"
+            
+            
     vals = ""
 time.sleep(1)
