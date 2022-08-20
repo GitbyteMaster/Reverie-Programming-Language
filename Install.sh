fe69d0a3814661219377c0a8161d6827fceed087
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-08-19 20:04:37 -0400

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -1,13 +1,39 @@
-echo Installing Reverie..
-mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/
-mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Reverie.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Reverie.py
-mkdir /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/
+echo "Creating Directories.."
+if ! [ -d "/Users/$USER/Reverie" ];then
+    mkdir "/Users/$USER/Reverie"
+fi
+if ! [ -d "/Users/$USER/Reverie/src" ];then
+    mkdir "/Users/$USER/Reverie/src"
+fi
 
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/error.txt > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/error.txt
-echo empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/cmdobj.txt
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/context.py > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/context.py
-echo empty > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/toexec.txt
-curl https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/main/main/Assetlist/revexec.sh > /Users/$USER/Desktop/Reverie-Programming-Language-main/main/Assetlist/revexec.sh
+echo "Creating Files.."
+echo > "/Users/$USER/Reverie/src/run"
 
-echo Installation Finished! Thank You for Downloading the Reverie Programming Language!
+echo "Downloading Files.."
+curl -s "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/main.py" > "/Users/$USER/Reverie/main.py"
+curl -s "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/func.sh" > "/Users/$USER/Reverie/src/func.sh"
+curl -s "https://raw.githubusercontent.com/GitbyteMaster/Reverie-Programming-Language/Reverie3%2B/build/src/error.sh" > "/Users/$USER/Reverie/src/error.sh"
+
+status="Installation Finished! Thank You for Downloading the Reverie Programming Language!"
+if ! [ -d "/Users/$USER/Reverie" ];then
+    status="Failed to Create '/Users/$USER/Reverie'"
+fi
+if ! [ -d "/Users/$USER/Reverie/" ];then
+    status="Failed to Create '/Users/$USER/Reverie/src'"
+fi
+
+
+if ! [ -f "/Users/$USER/Reverie/main.py" ];then
+    status="Failed to Create '/Users/$USER/Reverie/main.py'"
+fi
+
+if ! [ -f "/Users/$USER/Reverie/src/run" ];then
+    status="Failed to Create '/Users/$USER/Reverie/src/run'"
+fi
+if ! [ -f "/Users/$USER/Reverie/src/func.sh" ];then
+    status="Failed to Create '/Users/$USER/Reverie/src/func.sh'"
+fi
+if ! [ -f "/Users/$USER/Reverie/src/run/error.sh" ];then
+    status="Failed to Create '/Users/$USER/Reverie/src/error.sh'"
+fi
+echo $status
