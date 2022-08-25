68314465639f360eb50e5053241dfc82b81e96fd
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-08-25 14:30:22 -0400

    Add files via upload

--- build/src/func.bat
+++ build/src/func.bat
@@ -5,14 +5,14 @@ IF "%1"=="1" (
   IF "%~3"=="" (
     CALL C:\Users\%USERNAME%\Reverie\src\error.bat 3 %2
   ) ELSE (
-    IF "%3"=="-d" (
+    IF %3 =="-d" (
       IF "%~5"=="" (
         ECHO %4
       ) ELSE (
         CALL C:\Users\%USERNAME%\Reverie\src\error.bat 1 %2
       )
     )
-    IF "%3"=="-f" (
+    IF %3 =="-f" (
       IF "%~6"=="" (
         IF EXIST "%5" (
           ECHO "%4" > "%5"
@@ -26,7 +26,7 @@ IF "%1"=="1" (
   )
 )
 REM pause([int])
-if "%1"=="2" (
+IF %1 =="2" (
   IF "%~4"=="" (
     TIMEOUT %3
     IF NOT ERRORLEVEL 0 (
@@ -38,7 +38,7 @@ if "%1"=="2" (
   )
 )
 REM open(str)
-IF "%1"=="3" (
+IF %1 =="3" (
   IF "%~4"=="" (
     START %3
     IF NOT ERRORLEVEL 0 (
@@ -55,8 +55,8 @@ IF "%1"=="4" (
   ) ELSE (
     IF "%3"=="-d" (
       IF "%~6"=="" (
-        IF EXIST %4 (
-	  IF EXIST %5 (
+        IF EXIST "%4" (
+	  IF EXIST "%5" (
             MOVE /Y %4 %5
           ) ELSE (
             CALL C:\Users\Reverie\src\error.bat 4 %2
@@ -67,8 +67,8 @@ IF "%1"=="4" (
       )
     IF "%3"=="-r" (
       IF "%~6"=="" (
-        IF EXIST %4 (
-	  IF NOT EXIST %5 (
+        IF EXIST "%4" (
+	  IF NOT EXIST "%5" (
             REN %4 %5
           ) ELSE (
             CALL C:\Users\Reverie\src\error.bat 4 %2
