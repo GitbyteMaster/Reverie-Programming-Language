36216f4a133ef98f4846e4dda14c25ef4ca85fa4
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-08-25 14:05:17 -0400

    Add files via upload

--- build/main.py
+++ build/main.py
@@ -7,7 +7,13 @@ import sys
 import platform
 
 script = []
-for line in open(open(f"/Users/{getpass.getuser()}/Reverie/src/run", "r+").read(),"r+"):
+run = open(f"/Users/{getpass.getuser()}/Reverie/src/run", "r").read()
+pth = ""
+mainum = 0
+while not mainum == run.index(".rev")+4:
+    mainum += 1
+    pth = f"{pth}{run[mainum-1]}"
+for line in open(pth, "r"):
     script.append(line)
 vals = ""
 def paramcollect():
@@ -29,15 +35,13 @@ def paramcollect():
         if platform.system() != "Windows":
             os.system("sh /Users/{getpass.getuser()/Reverie/src/error.sh 5 {mainum}")
         else:
-            os.system("CALL C:\Users\{getpass.getuser()}\Reverie\src\error.sh 5 {mainum}")
+            os.system("CALL C:\\Users\{getpass.getuser()}\Reverie\src\error.sh 5 {mainum}")
 mainum = 0
 literal = False
-if platform.system() != "":
+if platform.system() != "Windows":
     os.system("clear")
 else:
     os.system("cls")
-if platform.system() != "Darwin":
-    os.system(f"title {sdir[filen-1]}")
 note = False
 var = []
 data = []
@@ -61,10 +65,11 @@ while not mainum == len(script):
         # Call function through engine (func.bat/sh)
         if callnum != 0:
             paramcollect()
+            print(vals)
             if platform.system() != "Windows":
                 os.system(f"sh /Users/$USER/Reverie/src/func.sh {callnum} {mainum} {vals}")
             else:
-                os.system(f"CALL C:\Users\%USERNAME\Reverie\func.bat {callnum} {mainum} {vals}")
+                os.system(f"CALL C:\\Users\%USERNAME%\Reverie\func.bat {callnum} {mainum} {vals}")
             
         # Functions in Python
         if "var " in line and "=" in line:
